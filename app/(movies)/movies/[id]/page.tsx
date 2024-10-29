d7baee2aff9c7ed130d1d314982dbbffefec3679
양도승 <157574745+yangdoseung@users.noreply.github.com>
2024-10-29 17:07:39 +0900

    깃허브 버그 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from "react";
-<<<<<<< HEAD
-import { API_URL } from "../../../(home)/page";
-=======
->>>>>>> a83c323 (hotpix:: movie data & CSS_Module 수정)
+
 import MovieInfo from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
