a1f2ced111ac2959e155c9fa85ec26d2d09c219f
양도승 <157574745+yangdoseung@users.noreply.github.com>
2024-10-29 16:58:41 +0900

    hotpix:: movie data & CSS_Module 수정

--- app/components/movie-info.tsx
+++ app/components/movie-info.tsx
@@ -1,5 +1,9 @@
 import { API_URL } from "../(home)/page";
+<<<<<<< HEAD
 
+=======
+import potato from "../../styles/movie-info.module.css";
+>>>>>>> a83c323 (hotpix:: movie data & CSS_Module 수정)
 async function getMovie(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(`${API_URL}/${id}`);
@@ -8,5 +12,25 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
+<<<<<<< HEAD
   return <h6>{JSON.stringify(movie)}</h6>;
+=======
+  return (
+    <div className={potato.container}>
+      <img
+        src={movie.poster_path}
+        className={potato.poster}
+        alt={movie.title}
+      />
+      <div className={potato.info}>
+        <h1 className={potato.title}>{movie.title}</h1>
+        <h3>⭐️{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
+>>>>>>> a83c323 (hotpix:: movie data & CSS_Module 수정)
 }
--- app/components/movie-videos.tsx
+++ app/components/movie-videos.tsx
@@ -1,5 +1,9 @@
 import { API_URL } from "../(home)/page";
+<<<<<<< HEAD
 
+=======
+import styles from "../../styles/movie-viedos.module.css";
+>>>>>>> a83c323 (hotpix:: movie data & CSS_Module 수정)
 async function getVideos(id: string) {
   console.log(`Fatching viedos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -10,5 +14,20 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
+<<<<<<< HEAD
   return <h6>{JSON.stringify(videos)}</h6>;
+=======
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={videos.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
+>>>>>>> a83c323 (hotpix:: movie data & CSS_Module 수정)
 }
